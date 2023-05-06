dcf690cede54e9802401f8649fd07512cb85652f
Ярослав Аверин <averinj@mail.ru>
2023-05-07 01:01:04 +0300

    added land and uniq blocks

--- assets/js/app.js
+++ assets/js/app.js
@@ -66,8 +66,8 @@ for (let i = 0; i < navLinks.length; i++) {
 // slick
 $(document).ready(function(){
 	$('.media__slider__inner').slick({
-		arrows:false,
-		dots:false,
+		arrows:true,
+		dots:true,
 		slidesToShow:1,
 		autoplay:true,
 		speed:1000,
